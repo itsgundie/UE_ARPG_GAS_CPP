be5d37ef1d2ba04bfe9390e47711741b1681dc16
itsGundie <49826242+itsgundie@users.noreply.github.com>
2025-01-16 19:27:26 +0200

    setup player's camera components and default values

--- Source/UE_ARPG_GAS_CPP/Public/Characters/ARPGHeroCharacter.h
+++ Source/UE_ARPG_GAS_CPP/Public/Characters/ARPGHeroCharacter.h
@@ -6,6 +6,9 @@
 #include "Characters/ARPGBaseCharacter.h"
 #include "ARPGHeroCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 /**
  * 
  */
@@ -13,7 +16,17 @@ UCLASS()
 class UE_ARPG_GAS_CPP_API AARPGHeroCharacter : public AARPGBaseCharacter
 {
 	GENERATED_BODY()
-	
+public:
+	AARPGHeroCharacter();
 protected:
 	virtual void BeginPlay() override;
+
+private:
+#pragma region Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+#pragma endregion
 };
